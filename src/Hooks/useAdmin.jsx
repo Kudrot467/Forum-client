09c560ca6e8b42dea91c791b9792d22972c0c665
Kudrot467 <kkudrotekhoda@gmail.com>
2023-11-30 22:51:41 +0600

    readme file modified

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -5,12 +5,12 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
 
-    const {user,loading} = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
     const axiosSecure=useAxiosSecure();
 
    const {data:isAdmin,isPending: isAdminLoading}=useQuery({
     queryKey:[user?.email,'isAdmin'],
-    enabled:!loading,
+    enabled:!!user,
     queryFn:async()=>{
         const res=await axiosSecure.get(`/users/admin/${user?.email}`)
         return res.data?.admin;
